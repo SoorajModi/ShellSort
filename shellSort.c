8fe1137c12e4a3c737acdc99ff02076e71492d3f
Sooraj Modi <soomodi@gmail.com>
2019-03-26 19:20:45 -0400

    shell sort is fully functional

--- shellSort.c
+++ shellSort.c
@@ -3,26 +3,46 @@
 #include <string.h>
 #include <time.h>
 
-void generateRandomArray(int** randNumArr);
+void generateRandomArray(int** numArr);
+void shellSort(int** numArr, int num);
 
 int main(void){
   printf("Beginning the Shell Sort Algorithm in C\n");
 
   printf("Generating an array of random numbers\n");
-  int* randNumArr = NULL;
-  generateRandomArray(&randNumArr);
+  int* numArr = NULL;
+  generateRandomArray(&numArr);
+  printf("Successfully generated an array of random numbers\n");
 
+  printf("Beginning shell sort\n");
+  shellSort(&numArr, 1000);
+  printf("Successfully sorted array\n");
 
-  if(randNumArr != NULL) free(randNumArr);
+  if(numArr != NULL) free(numArr);
   printf("Successfully exiting program\n");
   return 0;
 }
 
-void generateRandomArray(int** randNumArr){
-  *randNumArr = malloc(sizeof(int)*1001);
+void shellSort(int** numArr, int arrLen){
+  for(int gap = arrLen/2; gap > 0; gap /= 2){
+    for(int i = gap; i < arrLen; i++){
+      int temp = (*numArr)[i];
+      int j = 0;
+
+      for(j = i; j >= gap && (*numArr)[j - gap] > temp; j--){
+        (*numArr)[j] = (*numArr)[j-gap];
+      }
+
+      (*numArr)[j] = temp;
+    }
+  }
+}
+
+void generateRandomArray(int** numArr){
+  *numArr = malloc(sizeof(int)*1001);
   srand(time(NULL));
 
   for(int i = 0; i < 1000; i++){
-    (*randNumArr)[i] = rand() % (1000 + 1);
+    (*numArr)[i] = rand() % (1000 + 1);
   }
 }
