f6083a84541494201abe1e0020f2b3ca197e0a51
Sooraj Modi <modis@fred.socs.uoguelph.ca>
2019-04-01 14:57:32 -0400

    added opening and closing file functionality

--- shellSort.f95
+++ shellSort.f95
@@ -7,6 +7,28 @@ program main
     implicit none
     real :: arrLen;
     integer, allocatable :: numArr(:)
+    character (len = 30) :: fileName
+    logical :: fileExist
     
-    write(*,*)"Beginning the Shell Sort Algorithm in Fortran"
+    write(*,*)'Beginning the Shell Sort Algorithm in Fortran'
+    
+    write(*,*)'Enter the name of the file of integers to be sorted:'
+    read(*,'(A)')fileName
+    
+    inquire(file=fileName, exist=fileExist)
+    if(.NOT. fileExist) stop 'File does not exist, aborting'
+    
+    open(unit=9,file=fileName)
+    
+    !if (fileExist) then
+    !   open(unit=9,file=fileName)
+    !else
+    !   write (*,*) 'File does not exist, aborting'
+    !   stop		!I prob should not do this
+    !end if
+    
+    
+    
+    !write (9,*) 'This is an example output to file'
+    close (9)
 end program
