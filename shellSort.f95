f1731c54573a5f8eb142153aa247a11119df1ef1
Sooraj Modi <modis@fred.socs.uoguelph.ca>
2019-03-31 22:22:21 -0400

    initial commit for shellsort fortran algorithm

---
+++ shellSort.f95
@@ -0,0 +1,12 @@
+! Sooraj Modi
+! CIS 3190 - Assignment 4
+! modis@uoguelph.ca
+! 0965941
+
+program main
+    implicit none
+    real :: arrLen;
+    integer, allocatable :: numArr(:)
+    
+    write(*,*)"Beginning the Shell Sort Algorithm in Fortran"
+end program
