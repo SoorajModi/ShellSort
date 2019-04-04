37f272c85f11fa124fa0254c54121afe7a04538d
Sooraj Modi <modis@fred.socs.uoguelph.ca>
2019-04-04 16:47:57 -0400

    added timer functionality in python file

--- shellSort.py
+++ shellSort.py
@@ -3,6 +3,9 @@
 # modis@uoguelph.ca
 # CIS 3190 - Assignment 4
 
+import time
+import random
+
 def readListFromFile(numList):
 	fileName = raw_input("Enter the name of the file to be read: ");
 	fp = open(fileName, "r");
@@ -17,10 +20,20 @@ def writeListToFile(numList):
 		for item in numList:
 			fp.write("%s" % item)
     fp.close()
-    print("Successfully printed array to file");	
+    print("Successfully printed array to file");
+    
+def shellSort(numList):
+	print("Beginning shell sort algorithm")
+	print("Beginning timer")
+	begin = time.time()
+	
+	print("Stopping timer")
+	end = time.time()
+	print("Total algorithm run time: ", end-begin, "seconds")
 
 print("Beginning shell sort algorithm in python")
 numList = list();
 readListFromFile(numList)
+shellSort(numList)
 writeListToFile(numList)
 print("Successfully exiting program")
